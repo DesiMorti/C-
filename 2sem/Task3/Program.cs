9c18c11f5d937d2002208915f50bcb3e1266ac42
Desi <desimorti@gmail.com>
2022-10-07 14:01:19 +1000

    Udp dz

---
+++ 2sem/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите день недели(число): ");
+int a = int.Parse(Console.ReadLine()!);
+
+if(a < 1 || a > 7) {
+    Console.WriteLine("Недопустимое число");
+    return;
+}
+if(a >= 6) {
+    Console.WriteLine("Выходной день");
+}
+else {
+    Console.WriteLine("Рабочий день");
+}
