8a56e68ff74a8c0c9986ac481c8c88a7237b8dbc
Desi <desimorti@gmail.com>
2022-10-12 20:34:53 +1000

    fix Task3

--- 3sem/task3/Program.cs
+++ 3sem/task3/Program.cs
@@ -9,7 +9,7 @@ Console.Write($"Введите число N: ");
 int.TryParse(Console.ReadLine()!, out N);
 
 void Cube(int num) {
-    Console.Write($"Таблица кубров: ");
+    Console.Write($"Таблица кубов: ");
     for(int i = 1; i <= num; i++) {
         int c = (int)Math.Pow(i,3);
         Console.Write($"{c} ");
