14ba8826c70be7b851f02369972bc1d9b9c96de3
Desi <desimorti@gmail.com>
2022-10-13 12:41:49 +1000

    udp exm13, 4sem

--- 4sem/task1/Program.cs
+++ 4sem/task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+    3, 5 -> 243 (3⁵)
+    2, 4 -> 16
+*/
+
+Console.Clear();
+int A;
+int B; 
+int result;
+
+Console.Write("Введите число А: ");
+int.TryParse(Console.ReadLine()!, out A);
+Console.Write("Введите число B: ");
+bool flag = int.TryParse(Console.ReadLine()!, out B);
+
--- Lessons/Example013_Switch-case/Program.cs
+++ Lessons/Example013_Switch-case/Program.cs
@@ -6,23 +6,33 @@
 Console.Clear();
 int x;
 Console.Write($"Введите номер четверти: ");
-int.TryParse(Console.ReadLine()!, out x);
+bool flag = int.TryParse(Console.ReadLine()!, out x);
 
-switch (x) {
-    case 1:     //if
-        Console.Write($"X > 0 Y > 0");
-        break;
-    case 2:     //if
-        Console.Write($"X < 0 Y > 0");
-        break;
-    case 3:     //if
-        Console.Write($"X > 0 Y < 0");
-        break;
-    case 4:     //if
-        Console.Write($"X < 0 Y < 0");
-        break;
-    default:    //else
-        Console.Write($"Неверный ввод");
-        break;
+if (!flag)
+{
+    Console.Write("Неверный ввод");
 }
+else
+{
+    switch (x)
+    {
+        case 1:     //if
+            Console.Write($"X > 0 Y > 0");
+            break;
+        case 2:     //if
+            Console.Write($"X < 0 Y > 0");
+            break;
+        case 3:     //if
+            Console.Write($"X > 0 Y < 0");
+            break;
+        case 4:     //if
+            Console.Write($"X < 0 Y < 0");
+            break;
+        default:    //else
+            Console.Write($"Неверный ввод");
+            break;
+    }
+}
+
+
 
