99cff263e41452f164383cd5091ef2067eddcb29
Desi <desimorti@gmail.com>
2022-10-18 17:20:36 +1000

    upd sem5

--- 5sem/task2/Program.cs
+++ 5sem/task2/Program.cs
@@ -4,3 +4,58 @@
     [-4, -6, 89, 6] -> 0
 */
 
+Console.Clear();
+int N;
+Console.Write("Укажите размер массива: ");
+bool flag = int.TryParse(Console.ReadLine()!, out N);
+
+if (N < 1 || !flag)
+{
+    Console.Write("Неверный ввод");
+    return;
+}
+
+int[] FillArray(int array)
+{
+    int[] collection = new int[array];
+    int index = 0;
+    while (index < array)
+    {
+        collection[index] = new Random().Next(-99, 100);
+        index++;
+    }
+    return collection;
+}
+
+int OddSumm(int[] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+        {
+            summ += array[i];
+        }
+    }
+    return summ;
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < count - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+
+int[] arr = FillArray(N);
+PrintArray(arr);
+Console.Write($" - сумма нечетных чисел: {OddSumm(arr)}");
--- 5sem/task3/Program.cs
+++ 5sem/task3/Program.cs
@@ -3,3 +3,58 @@
     [3 7 22 2 78] -> 76
 */
 
+Console.Clear();
+int N;
+Console.Write("Укажите размер массива: ");
+bool flag = int.TryParse(Console.ReadLine()!, out N);
+
+if (N < 1 || !flag)
+{
+    Console.Write("Неверный ввод");
+    return;
+}
+
+double[] FillArray(int size)
+{
+    double[] collection = new double[size];
+    int index = 0;
+    while (index < size)
+    {
+        collection[index] = new Random().NextDouble()*10-10;
+        index++;
+    }
+    return collection;
+}
+
+double MaxMinDiff(double[] array)
+{
+    double min = array[0]; 
+    double max = array[0]; 
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+        else if (array[i] > max) max = array[i];
+    }
+
+    return max - min;
+}
+
+void PrintArray(double[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < count - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+
+double[] arr = FillArray(N);
+PrintArray(arr);
+Console.Write($" - разница между максимальным и минимальным элементами: {MaxMinDiff(arr)}");
